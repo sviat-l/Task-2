35d3b4bce0037b140cc31e3c0a29f90de54c3428
shaman4ik14 <86052333+shaman4ik14@users.noreply.github.com>
2021-12-15 12:18:25 +0200

    Add files via upload
    function for working with field

--- ship.py
+++ ship.py
@@ -1,11 +1,71 @@
 def get_field():
-    pass
+    def create_field():
+        field = [['_' for i in range(10)] for _ in range(10)]
+        return field
+    def pos_lodka():
+        """
+        You give start pos for your LODKA (довжина - 3 по горизонталі)
+        """
+        while True:
+            print('Print coordinate: ')
+            pos = list(map(str, input().split()))
+            if int(pos[1]) < 7 and int(pos[0]) < 10:
+                pos = [int(element) for element in pos]
+                return pos
+            elif pos == "quit":
+                return pos
+    def current_list(field):
+        number_of_boates = 0
+        print('\t')
+        column = 0
+        print(end='  ')
+        for elements in range(10):
+            column += 1
+            if column == 10:
+                print('{0:^5}'.format(elements))
+                break
+            print('{0:^5}'.format(elements), end='')
+        row = 0
+        for elements in field:
+            print(f'{row} {elements}')
+            row += 1
+        while number_of_boates <= 3:
+            pos = pos_lodka()
+            if pos == "quit":
+                return  None
+            else:
+                for _ in range(3):
+                    field[pos[0]][pos[1]+_] = 'O'
+                number_of_boates += 1
+                print('\t')
+                column = 0
+                print(end='  ')
+                for elements in range(10):
+                    column += 1
+                    if column == 10:
+                        print('{0:^5}'.format(elements))
+                        break
+                    print('{0:^5}'.format(elements), end='')
+                row = 0
+                for elements in field:
+                    print(f'{row} {elements}')
+                    row += 1
+        return field
+    result = current_list(create_field())
+    return result
+
+
+
+
+
+
 
 def print_field():
     pass
 
 def analyse_field():
     pass
+
 
 def checking_bullseye(field_enemy: list, field_shots: set, shot: tuple):
     horiz = shot[0]
